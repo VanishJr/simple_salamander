1a6e7b5e0c67a2bc63b1a07b2a0f8fc0336d8c0c
Ivan Logutov <foreverivan2004@gmail.com>
2024-10-15 13:06:03 +0200

    Added to the project node.js, react, scss. Setted the enviroment and created html and css file.

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Example route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
